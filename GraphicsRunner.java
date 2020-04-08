09a17dd36ac2cef0f96210e99460cfccef13b15f
Admin <Admin@Amazon-echo-dot-2.T-mobile.com>
2020-04-08 12:16:43 -0500

    added some stuff to graphics

--- GraphicsRunner.java
+++ GraphicsRunner.java
@@ -1,7 +1,66 @@
-public class GraphicsRunner
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+
+public class GraphicsRunner extends JPanel implements MouseListener
 {
-	public GraphicsRunner()
+	private Board game;
+	private ImageIcon bg;
+	private ImageIcon logo;
+	public GraphicsRunner(Board g)
 	{
-		//nothing yet
+		 JFrame frame = new JFrame("POWERGRID");
+		 frame.setBackground(Color.white);
+		 setVisible(true);
+		 //frame.getContentPane().setBackground(Color.orange);
+		 bg = new ImageIcon("bg.jpg");
+		 logo = new ImageIcon("logo.png");
+		 addMouseListener(this);
+		 
+		 frame.add(this);
+		 frame.setSize(1920,1080);
+		 frame.setResizable(true);
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 frame.setVisible(true);
+	}
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		System.out.println(e.getX());
 	}
 }
